c98e5cfee083f3bdd623af5cf90da148b4453274
shchyrska_ya <mariia.shchyrska@oa.edu.ua>
2023-09-19 13:39:38 +0300

    Context shifting

---
+++ src/PageTurners.Core/Context/PageTurnersContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PageTurners.Core.Context
+{
+    public class PageTurnersContext : IdentityDbContext
+    {
+        public PageTurnersContext(DbContextOptions<PageTurnersContext> options)
+            : base(options)
+        {
+        }
+    }
+}
--- src/PageTurners.WebApp/Program.cs
+++ src/PageTurners.WebApp/Program.cs
@@ -1,17 +1,17 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using PageTurners.WebApp.Data;
+using PageTurners.Core.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+builder.Services.AddDbContext<PageTurnersContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+    .AddEntityFrameworkStores<PageTurnersContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
