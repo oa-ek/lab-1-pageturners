d8c3d1af9fb605a6c8d68d517a3e55e9378d357d
kravchenkoolya <olha.kravchenko@oa.edu.ua>
2023-09-19 16:49:40 +0200

    Add Entities

---
+++ src/PageTurners.Core/Entities/ModeratorReview.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageTurners.Core.Entities
+{
+    public class ModeratorReview
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Review_Id { get; set; }
+        public int Request_Id { get; set; }
+        public int Moderator_Id { get; set; }
+        public string Review_Comment { get; set; }
+        public string Review_Status { get; set; }
+
+        public virtual BookRequest Request { get; set; }
+        public virtual User Moderator { get; set; }
+    }
+
+}
