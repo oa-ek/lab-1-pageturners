f78455e3e9c04a87bc66ed4b609e238ba22cd357
shchyrska_ya <mariia.shchyrska@oa.edu.ua>
2023-12-12 11:25:16 +0200

    Repositories

---
+++ src/PageTurners.Application/Repositories/IBookRepository.cs
@@ -0,0 +1,14 @@
+﻿using PageTurners.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageTurners.Application.Repositories
+{
+	public interface IBookRepository : IBaseRepository<Book>
+	{
+		Task<IEnumerable<Book>> FindByTitlePartSync(string text);
+	}
+}
